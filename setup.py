84280a0f41e25624a0c6e6f858ccd6a38a4d9eb6
Dawn Dream/xm2020 <64729945+Dawn-Dream@users.noreply.github.com>
2025-06-02 02:59:15 +0800

    feat: 优化文件比对算法

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezIncrementalBackup",
-    version="0.1.2b12",
+    version="0.1.2b13",
     packages=find_packages(),
     install_requires=[
         'click',
